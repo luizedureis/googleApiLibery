48cf7884137a282cf4808cb96a7a30051da6c90c
Luiz Tavares <102179209+LuizTavres@users.noreply.github.com>
2023-03-17 15:18:45 -0300

    delete ok

--- drive/file_class.py
+++ drive/file_class.py
@@ -9,7 +9,6 @@ class file(Google):
         # puxando nome do arquivo que será substituido
         self.__file = file_name
         self.service = self.generate_service()
-
     def generate_service(self):
         #Keys para capturar pastas drive
         SCOPES = ['https://www.googleapis.com/auth/drive.file']
@@ -148,14 +147,35 @@ class file(Google):
                 inicial = True
         return query
       
-    def teste_listagem_itens(self,folder_id):
-        # query para filtrar a pasta pai e o nome do arquivo target
-        query = f"parents = '{folder_id}' and name = '{self.__file[0]}'"
-        print(query)
-        # localizando file, pelo correto irá retornar apenas um item
-        file = self.service.files().list(q=query).execute()
-        print(file)
-        # self.service.files().delete(fileId=fileid).execute()
+    def compatibility_test(self,folder_id):
+        '''
+        This class files, do not have compatibility with some files, you can use this function to get a list with the name of files compatible with the class\n
+        You can call the function like:
+        >>> file().compatibility_test(folder_id)\n
+        The function will return a dict:
+        >>> [{file1:id},{file2:id},{file3:id}] \n
+        Or, you can check if an especific file is compatible:
+        >>> file(namefile).compatibility_teste(folder_id)\n
+        The functino will return a bool:
+        >>> True \n
+        or 
+        >>> False
+        '''
+        self.__id = 1033317
+        if len(self.__file)==0:
+            query = f"parents = '{folder_id}'"
+            files = self.service.files().list(q=query).execute()
+            lista=[]
+            for file in files['files']:
+                lista.append({file['name']:file['id']})
+            return lista
+        elif len(self.__file)>0:
+            query = f"parents = '{folder_id}' and name = '{self.__file[0]}'"
+            files = self.service.files().list(q=query).execute()
+            if len(files['files'])==0:
+                return False
+            else:
+                return True
 
 
     def upload_file(self,folder_id,mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',convert=None):
@@ -181,6 +201,7 @@ class file(Google):
         https://developers.google.com/drive/api/guides/manage-uploads?hl=pt-br#importing_to_google_docs_types_wzxhzdk8wzxhzdk9
         \n
         OBS:Do not pass file name with ".", the code split the "." to get the file extension.
+        ex:'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' to 'application/vnd.google-apps.spreadsheet'
         '''
         self.__id=602316
         # gerando file metadata com a opção de converter ou não
@@ -198,3 +219,92 @@ class file(Google):
         self.service.files().update(
             fileId     = file['id'],
             addParents = folder_id).execute()
+        
+    def upload_files(self,folderid=None,convert=None,ids=False):
+        '''
+        Upload to the same folder->\n
+        To upload diferent files to the same folder, you will call the function like:
+        >>> file({"file1.ex":mimeType},{"file2.ex":mimeType}).upload_files(folderid)
+        In this exemple ".ex" is just an exemple.
+        \n
+        If you want to convert your file use:\n
+        convert = mimetype_new
+        \n
+        Upload files to diferent folders->\n
+        To upload diferent files to the same folder, you will call the function like:
+        \n
+        >>> file({"file1.ex":[mimeType,folder_id]},{"file2.ex":[mimetype,folder_id]})\n
+        If you pass mimeType like "-" the function will consider like a xlsx file\n
+        If you pass convert like "-" the function will convert to google sheets
+        '''
+        self.__id = 1028317
+        if folderid != None:
+            # rodando dic de arquivos
+            lista=[]
+            for file_meta in self.__file:
+                # puxando nome e mimeType de cada arquivo
+                for name_file in file_meta:
+                    mimeType = file_meta[name_file]
+                    if mimeType=="-":
+                        mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+                    # gerando file metadata com a opção de converter ou não para outro tipo de arquivo google
+                    if   convert == None: file_metadata = {'name':str(name_file).split(".")[0]}
+                    elif convert == "-":  file_metadata = {"name":str(name_file).split(',')[0],'mimeType':'application/vnd.google-apps.spreadsheet'}
+                    elif convert != None: file_metadata = {"name":str(name_file).split(',')[0],'mimeType':convert}
+                    media = MediaFileUpload(name_file,mimetype=mimeType,resumable = True)
+                    # gerando arquivo
+                    file = self.service.files().create(body    = file_metadata,
+                                                    media_body = media,
+                                                    fields     = 'id').execute()
+                    # atualizando arquivo e subindo na pasta do drive indicada
+                    self.service.files().update(
+                        fileId     = file['id'],
+                        addParents = folderid).execute()
+                    
+                    lista.append({name_file:file['id']})
+                    print(f'file {name_file} uploaded')
+            if ids==True:
+                return lista
+        elif folderid==None:
+            lista=[]
+            for file_meta in self.__file:
+                for name_file in file_meta:
+                    mimeType = file_meta[name_file][0]
+                    if mimeType=="-":
+                        mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                    folder_target = file_meta[name_file][1]
+
+                    # gerando file metadata com a opção de converter ou não para outro tipo de arquivo google
+                    if   convert == None: file_metadata = {'name':str(name_file).split(".")[0]}
+                    elif convert == "-":  file_metadata = {"name":str(name_file).split(',')[0],'mimeType':'application/vnd.google-apps.spreadsheet'}
+                    elif convert != None: file_metadata = {"name":str(name_file).split(',')[0],'mimeType':convert}
+                    media = MediaFileUpload(name_file, mimetype = mimeType, resumable = True)
+                    # gerando arquivo
+                    file = self.service.files().create(body    = file_metadata,
+                                                    media_body = media,
+                                                    fields     = 'id').execute()
+                    # atualizando arquivo e subindo na pasta do drive indicada
+                    self.service.files().update(
+                        fileId     = file['id'],
+                        addParents = folder_target).execute()
+                    lista.append({name_file:file['id']})
+                    print(f'file {name_file} uploaded')
+            if ids==True:
+                return lista
+    
+    def delete_file(self,*fileId):
+        '''
+        Function to delete file from drive, use the function like:
+        >>> file().delete_file(file_id)\n
+
+        To delete more than one file, use the function like:
+        >>> file().delete_file(file_id_1, file_id_2)
+        '''
+        self.__id = 304317
+        for id in fileId:
+            try:
+                self.service.files().delete(fileId=id).execute()
+            except:
+                Error(self.__id,'Del_file_error',id)
+                
